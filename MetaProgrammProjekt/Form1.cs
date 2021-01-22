f70c442d7e96126aea965456d495cd198a45bbad
jimmymeister98 <jimmy.neitzert@gmail.com>
2021-01-22 19:37:23 +0100

    Projektdateien hinzufügen.

---
+++ MetaProgrammProjekt/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static MetaProgrammProjekt.KonsolenHandler;
+
+namespace MetaProgrammProjekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         KonsolenHandler inputHandler = new KonsolenHandler();
+         inputHandler.WriteToConsole("code");
+        }
+    }
+}
---
+++ MetaProgrammProjekt/KonsolenHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace MetaProgrammProjekt
+{
+    class KonsolenHandler
+    {
+        public void WriteToConsole(string input)
+        {
+            Process p = new Process();
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "cmd.exe";
+            info.RedirectStandardInput = true;
+            info.UseShellExecute = false;
+
+            p.StartInfo = info;
+            p.Start();
+
+            using (StreamWriter sw = p.StandardInput)
+            {
+                if (sw.BaseStream.CanWrite)
+                {
+                    sw.WriteLine(input);
+                    
+                }
+            }
+            
+        }
+    }
+}
