ec60480d0f8eec381655406fe36bf503c0ff1197
jimmymeister98 <jimmy.neitzert@gmail.com>
2021-03-11 02:19:28 +0100

    Einlesen von Json implementiert und erstellen von .cs dateien

--- MetaProgrammProjekt/Form1.cs
+++ MetaProgrammProjekt/Form1.cs
@@ -7,10 +7,12 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static MetaProgrammProjekt.JsonToClass;
 using static MetaProgrammProjekt.KonsolenHandler;
-
+using System.Runtime.InteropServices;
 namespace MetaProgrammProjekt
 {
     public partial class Form1 : Form
@@ -20,10 +22,21 @@ namespace MetaProgrammProjekt
             InitializeComponent();
         }
 
+       
         private void button1_Click(object sender, EventArgs e)
         {
-         KonsolenHandler inputHandler = new KonsolenHandler();
-         inputHandler.WriteToConsole("code");
+            //var jsonText string = File.ReadAllText(@"./")
+            AllocConsole();
+            string pwd = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            Directory.SetCurrentDirectory(pwd);
+            JsonToClass JsonHandler = new JsonToClass();
+            JsonToClass.LoadJson("C:\\Users\\Jimmy Neitzert\\Downloads\\message.json");
+            
+            //KonsolenHandler inputHandler = new KonsolenHandler();
+            //inputHandler.WriteToConsole("dotnet new classlib -o C:\\Users\\Jimmy Neitzert\\source\\repos\\Testlauf"); //kann sein dass backslashes oder sonderzeichen mit backslash escaped werden müssen
         }
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
     }
 }
---
+++ MetaProgrammProjekt/JsonReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MetaProgrammProjekt
+{
+    class JsonToClass
+    {
+        public static void LoadJson(string Path)
+        {
+            JObject o1 = JObject.Parse(File.ReadAllText(Path)); //lese json ein
+            int objektCount = o1["umpleClasses"].Count();       //zähle Objekte der Json des Arrays "umpleClasses"
+            for (int i = 0; i < objektCount; i++)   
+               {
+                    string idteststring = (string) o1["umpleClasses"][i]["id"];  //gib alle id's aus
+                    Console.WriteLine(idteststring);
+                    using (FileStream fs = File.Create("C:\\Users\\Jimmy Neitzert\\Downloads\\"+idteststring+".cs"));
+               }
+            
+                
+
+        }
+    }
+
+    class JsonToRelation
+    {
+
+    }
+}
+
